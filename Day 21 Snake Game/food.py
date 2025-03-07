ec2605a9b9a9a571ea4273dbd7192a2811fd4f3b
Muhammad Ismaeel Saghar <85483258+MuhammadIsmaeel786@users.noreply.github.com>
2025-03-07 14:49:58 +0500

    day 20 and 21

---
+++ Day 21 Snake Game/food.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+import random
+# Food creation
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape('circle')
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color('orange')
+        self.speed('fastest')
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(x=random_x, y=random_y)
+        # to avoid duplication
+        self.refresh()
+    # Random place of food
+    def refresh(self):
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(x=random_x, y=random_y)
